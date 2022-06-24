208041b1a9f7dcf40f587aa3668a7e1763720e72
Kurt <91346063+Kpfeffer85@users.noreply.github.com>
2022-06-24 15:22:17 -0500

    Add files via upload

--- mongodb_test.py
+++ mongodb_test.py
@@ -1,5 +1,17 @@
+import certifi 
 from pymongo import MongoClient
-url = 'mongodb+srv://admin:admin@cluster0.urpvr.mongodb.net/?retryWrites=true&w=majority'
-client  = MongoClient(url)
-db  = client.pytech
-print(db.list_collection_names)
+url = "mongodb+srv://admin:admin@cluster0.urpvr.mongodb.net/pytech"
+ca= certifi.where()
+
+client = MongoClient(url, tlsCAFile = ca)
+db = client.pytech
+print('\n -- Pytech Collection List --')
+print(db.list_collection_names())
+
+
+input("\n\n  End of program, press any key to exit... ")
+
+
+
+
+
